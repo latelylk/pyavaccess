1ce6ef8721e3457a28e92c290006840c944d01f7
Pierce <33592564+latelylk@users.noreply.github.com>
2023-11-11 21:02:20 -0500

    add: HDMI matrix config

---
+++ pyavaccess/config/matrix_devices.py
@@ -0,0 +1,16 @@
+MatrixDevices = {
+    "4KMX42-H2A": {
+        "inputCount": 4,
+        "outputCount": 2,
+        "audioOutputs": [
+            "hdmiaudioout1",
+            "hdmiaudioout2",
+            "audioout1",
+            "spdifaudioout2",
+        ],
+        "EDIDParamCount": 11,
+        "maxDelayInMin": 30,
+        "irModeCount": 2,
+        "commandCount": 24,
+    }
+}
