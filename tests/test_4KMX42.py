5197ed07ab10f5a917f92e877ad7536734897b26
Pierce <33592564+latelylk@users.noreply.github.com>
2024-04-22 18:45:59 -0400

    add: empty command test

--- tests/test_4KMX42.py
+++ tests/test_4KMX42.py
@@ -1,4 +1,5 @@
 from pyavaccess import HDMIMatrixSerial
+import pytest
 
 # Port used in tests (requires live device)
 # Can be a url path "/dev/XYZ" or device name "COMx"
@@ -13,25 +14,6 @@ def test_create():
     assert matrix is not None
 
 
-def test_attributes():
-    """
-    Test the attributes of an HDMIMatrixSerial object
-    """
-    matrix = HDMIMatrixSerial(SERIAL_URL, "4KMX42-H2A")
-    assert matrix.inputs == 4
-    assert matrix.outputs == 2
-    assert matrix.audioOutputs == [
-        "hdmiaudioout1",
-        "hdmiaudioout2",
-        "audioout1",
-        "spdifaudioout2",
-    ]
-    assert matrix.prmEDIDCount == 11
-    assert matrix.maxDelay == 30
-    assert matrix.irModeCount == 2
-    assert matrix.commandCount == 24
-
-
 def test_communication():
     """
     Test communicating with the device
@@ -41,8 +23,19 @@ def test_communication():
     assert output == "VER 1.0.2"
 
 
+def test_sendEmptyString():
+    """
+    Test communicating with the device
+    """
+    matrix = HDMIMatrixSerial(SERIAL_URL, AV_DEVICE)
+
+    # getVer has a stable output to use for verifying communication
+    with pytest.raises(Exception):
+        matrix._SendData("")
+
+
 # For manual use instead of pytest
 if __name__ == "__main__":
     test_create()
-    test_attributes()
     test_communication()
+    test_sendEmptyString()
